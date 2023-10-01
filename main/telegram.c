66232e5e43060a400eba6d843a93d8ed1d1fef0f
Csete Balazs <csetebalazs2@gmail.com>
2023-10-01 17:14:20 +0200

    send as mediagroup

--- main/telegram.c
+++ main/telegram.c
@@ -9,25 +9,22 @@
 extern const uint8_t cert_pem_start[] asm("_binary_cert_pem_start");
 extern const uint8_t cert_pem_end[] asm("_binary_cert_pem_end");
 
-#define FORM_DATA_PREFIX_LENGTH FORM_DATA_BOUNDARY_LENGTH + 114
-#define FORM_DATA_SUFFIX_LENGTH FORM_DATA_BOUNDARY_LENGTH + TELEGRAM_CHAT_ID_LENGTH + FORM_DATA_BOUNDARY_LENGTH + 64
-
 static const char *CONTENT_TYPE_HEADER = "multipart/form-data; boundary=" FORM_DATA_BOUNDARY;
-static const char *URL = "https://api.telegram.org/bot" TELEGRAM_TOKEN "/sendPhoto";
-static const char *FORM_DATA_PREFIX = "--" FORM_DATA_BOUNDARY "\r\nContent-Disposition: form-data; name=\"photo\"; filename=\"image.jpg\"\r\nContent-Type: application/octet-stream\r\n\r\n";
+static const char *URL = "https://api.telegram.org/bot" TELEGRAM_TOKEN "/sendMediaGroup";
+static const char *FORM_DATA_PREFIX = "--" FORM_DATA_BOUNDARY "\r\nContent-Disposition: form-data; name=\"media\"\r\n\r\n[{\"type\":\"photo\",\"media\":\"attach://photo\"}]\r\n--" FORM_DATA_BOUNDARY "\r\nContent-Disposition: form-data; name=\"photo\"; filename=\"image.jpg\"\r\nContent-Type: application/octet-stream\r\n\r\n";
 static const char *FORM_DATA_SUFFIX = "\r\n--" FORM_DATA_BOUNDARY "\r\nContent-Disposition: form-data; name=\"chat_id\"\r\n\r\n" TELEGRAM_CHAT_ID "\r\n--" FORM_DATA_BOUNDARY "--\r\n";
 
 void construct_payload(uint8_t *body, size_t body_len, char **out, size_t *out_len)
 {
-  size_t payload_len = FORM_DATA_PREFIX_LENGTH + body_len + FORM_DATA_SUFFIX_LENGTH;
+  size_t payload_len = strlen(FORM_DATA_PREFIX) + body_len + strlen(FORM_DATA_SUFFIX);
 
   char *payload = malloc(payload_len);
   size_t offset = 0;
-  memcpy(payload + offset, FORM_DATA_PREFIX, FORM_DATA_PREFIX_LENGTH);
-  offset += FORM_DATA_PREFIX_LENGTH;
+  memcpy(payload + offset, FORM_DATA_PREFIX, strlen(FORM_DATA_PREFIX));
+  offset += strlen(FORM_DATA_PREFIX);
   memcpy(payload + offset, body, body_len);
   offset += body_len;
-  memcpy(payload + offset, FORM_DATA_SUFFIX, FORM_DATA_SUFFIX_LENGTH);
+  memcpy(payload + offset, FORM_DATA_SUFFIX, strlen(FORM_DATA_SUFFIX));
 
   *out = payload;
   *out_len = payload_len;
